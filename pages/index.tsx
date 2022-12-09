fd472e542872ea23545601257f01a7f2e29928f6
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-12-09 09:38:33 +0200

    Utilizing Incremental Static Generation (ISR)

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,7 @@ function HomePage(props: HomePageProps) {
 }
 
 export async function getStaticProps() {
+  console.log('(Re-)Generating...');
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString()) as HomePageProps;
@@ -28,6 +29,7 @@ export async function getStaticProps() {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
