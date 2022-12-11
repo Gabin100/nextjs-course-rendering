29f8fd9aa202c07efb1fdcfaa4b3993364e8588f
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-12-11 20:31:33 +0200

    using the useSWR NextJS

--- pages/last-sales.tsx
+++ pages/last-sales.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 interface SalesData {
   id: string;
@@ -8,36 +9,55 @@ interface SalesData {
 
 const LastSales = () => {
   const [sales, setSales] = useState<SalesData[]>();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    'https://jibuchat.firebaseio.com/sales.json',
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch('https://jibuchat.firebaseio.com/sales.json')
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales: SalesData[] = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: Number(data[key].volume),
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        console.log(error);
-      });
-  }, []);
+    if (data) {
+      const transformedSales: SalesData[] = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: Number(data[key].volume),
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch('https://jibuchat.firebaseio.com/sales.json')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales: SalesData[] = [];
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: Number(data[key].volume),
+  //         });
+  //       }
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       setIsLoading(false);
+  //       console.log(error);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>No data Found!</p>;
   }
 
-  if (!sales) {
-    return <p>No data Found!</p>;
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
